630e7baffaa28b81be102a1ffd8d221ae0134e37
Namratapatel216 <49944695+Namratapatel216@users.noreply.github.com>
2019-08-27 18:51:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -75,31 +75,96 @@ app.listen(appConfig.port, () => {
 });
 
 const server = http.createServer(app);
+// start listening to http server
+console.log(appConfig);
+server.listen(appConfig.port);
+server.on('error', onError);
+server.on('listening', onListening);
 
-//const server = "http://api.npatelproject.site";
+// end server listening code
 
-console.log(JSON.stringify(server));
 
 // socket io connection handler 
 const socketLib = require("./app/libs/socketLib");
 const socketServer = socketLib.setServer(server);
 
-mongoose.set('useCreateIndex', true);
+
+// end socketio connection handler
 
 
-mongoose.connection.on('error', function(err) {
-    console.log("database connection error");
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    logger.error(error.code + ' not equal listen', 'serverOnErrorHandler', 10)
+    throw error;
+  }
+
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      logger.error(error.code + ':elavated privileges required', 'serverOnErrorHandler', 10);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      logger.error(error.code + ':port is already in use.', 'serverOnErrorHandler', 10);
+      process.exit(1);
+      break;
+    default:
+      logger.error(error.code + ':some unknown error occured', 'serverOnErrorHandler', 10);
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  ('Listening on ' + bind);
+  logger.info('server listening on port' + addr.port, 'serverOnListeningHandler', 10);
+  let db = mongoose.connect(appConfig.db.uri,{ useMongoClient: true });
+}
+
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+  // application specific logging, throwing an error, or other logic here
+});
+
+
+/**
+ * database connection settings
+ */
+mongoose.connection.on('error', function (err) {
+  console.log('database connection error');
+  console.log(err)
+  logger.error(err,
+    'mongoose connection on error handler', 10)
+  //process.exit(1)
+}); // end mongoose connection error
+
+mongoose.connection.on('open', function (err) {
+  if (err) {
+    console.log("database error");
     console.log(err);
-});
+    logger.error(err, 'mongoose connection open handler', 10)
+  } else {
+    console.log("database connection open success");
+    logger.info("database connection open",
+      'database connection open handler', 10)
+  }
+  //process.exit(1)
+}); // enr mongoose connection open handler
 
-mongoose.connection.on('open',function(err){
-    if(err)
-    {
-        console.log("database error");
-        console.log(err);
-    }
-    else
-    {
-        console.log("database connection open success");
-    }
-});
+
+
+module.exports = app;
